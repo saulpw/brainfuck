c0979590cd984a29468af31e9383ef00c903fdfb
saul <saul@ef3b9ed0-3d08-0410-9d73-8659cd293b64>
2010-10-06 07:04:51 +0000

    cleaned up, test suite from the web
    git-svn-id: svn+ssh://pwanson.com/var/home/svn/sitelabor/projects/brainfuck@484 ef3b9ed0-3d08-0410-9d73-8659cd293b64

--- main.c
+++ main.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CHECK do { \
-    if (ip < program || ip > &program[ncode])  \
-        { printf("Bad Ip: %ld\n", ip - program); dump_state(); assert(0); } \
-    if (ptr < data || ptr > &data[4])  \
-        { printf("Bad Data Ptr: %ld\n", ptr - data); dump_state(); assert(0); } \
-} while (0)
-
 char data[30000] = { 0 };
 char *ptr = data;
 char program[30000] = { 0 };
 char *ip = program;
+int ncode = 0;
+int ndata = 0;
+int debug = 0;
+int verbose = 0;
 
 void dump_state()
 {
+    if (!verbose) return;
     int i;
+    if (debug) printf("\nIP=% 8ld  ", ip - program);
     printf("[% 8ld] Data[0-9] ", ptr - data);
     for (i=0; i < 10; i++)
         printf("% 3d ", data[i]);
@@ -24,11 +23,91 @@ void dump_state()
     printf("\n");
 }
 
+void check_state()
+{
+    if (ip < program || ip > &program[ncode])
+        { dump_state(); assert(0); }
+    if (ptr < data || ptr > &data[4])
+        { dump_state(); assert(0); }
+}
+
+
+void advance_data(int n)
+{
+    ptr += n;
+}
+
+void incr_data(int n)
+{
+    *ptr += n;
+}
+
+void out_comment()
+{
+    fprintf(stderr, "%c", *ip);
+}
+
+void out_data()
+{
+    printf("%c", *ptr);
+}
+
+void in_data()
+{
+    *ptr = getchar();
+}
+
+void begin_loop()
+{
+// all problems are the same problem, at their core
+    if (*ptr == 0)
+    {
+        // if the byte at the data pointer is zero, then instead of
+        // moving the instruction pointer forward to the next command,
+        // jump it forward to the command after the matching ]
+        // command*.
+        int n = 1;
+        do {
+            ip++;
+            check_state();
+            if (*ip == '[') {
+                n++;
+            } else if (*ip == ']') {
+                n--;
+            }
+        }
+        while (n != 0 || *ip != ']');
+    }
+}
+
+void end_loop()
+{
+    if (*ptr != 0)
+    {
+        // if the byte at the data pointer is nonzero, then instead of
+        // moving the instruction pointer forward to the next command,
+        // jump it back to the command after the matching [ command*.
+        int n = 1;
+        do {
+            ip--;
+
+            check_state();
+            if (debug) printf("%c", *ip);
+
+            if (*ip == '[')
+            {
+                n--;
+            } else if (*ip == ']') {
+                n++;
+            }
+        }
+        while (n != 0 || *ip != '[');
+    }
+}
+
 int
 main(int argc, const char *argv[])
 {
-    int ncode = 0;
-
     FILE *fp = fopen(argv[1], "r");
     
     if (fp == NULL)
@@ -44,95 +123,31 @@ main(int argc, const char *argv[])
         ip++;
         ncode++;
     }
+    fclose(fp);
 
     ip = program;
 
     while (*ip != 0)
     {
-        // in every moment, what is the right thing to do?
+        if (debug) printf("%c", *ip);
 
-        //printf("%c", *ip);
         switch (*ip)
         {
         case '#': dump_state(); break;
-        case '>':
-            ++ptr;
-            break;
-        case '<':
-            --ptr;
-            break;
-        case '+':
-            ++*ptr;
-            break;
-        case '-':
-            --*ptr;
-            break;
-        case '.':
-            printf("%c", *ptr);
-            break;
-        case ',':
-            *ptr = getchar();
-            break;
-        case '[':
-        // all problems are the same problem, at their core
-            if (*ptr == 0)
-            {
-                // if the byte at the data pointer is zero, then instead of
-                // moving the instruction pointer forward to the next command,
-                // jump it forward to the command after the matching ]
-                // command*.
-                int n = 1;
-                do {
-                    ip++;
-                    CHECK;
-                    if (*ip == '[') {
-//                        printf(" (n=%d) ", n);
-                        n++;
-                    } else if (*ip == ']') {
-//                        printf(" (n=%d) ", n);
-                        n--;
-                    }
-                }
-                while (n != 0 || *ip != ']');
-                printf(" end ");
-            }
-            break;
-        case ']':
-            if (*ptr != 0)
-            {
-                // if the byte at the data pointer is nonzero, then instead of
-                // moving the instruction pointer forward to the next command,
-                // jump it back to the command after the matching [ command*.
-                int n = 1;
-                do {
-                    ip--;
-
-                    CHECK;
-//                    printf("%c", *ip);
-
-                    if (*ip == '[')
-                    {
-                        n--;
-//                        printf(" (n=%d) ", n);
-                    } else if (*ip == ']') {
-                        n++;
-//                        printf(" (n=%d) ", n);
-                    }
-                }
-                while (n != 0 || *ip != '[');
-//                printf(" loop ");
-            }
-            break;
-        default:
-            // ignore it
-            break;
+        case '>': advance_data(1); break;
+        case '<': advance_data(-1); break;
+        case '+': incr_data(1); break;
+        case '-': incr_data(-1); break;
+        case '.': out_data(); break;
+        case ',': in_data(); break;
+        case '[': begin_loop(); break;
+        case ']': end_loop(); break;
+        default:  out_comment(); break;
         };
 
         ip++;
-        CHECK;
+        check_state();
     }
-
-    fclose(fp);
 
     return 0;
 }
