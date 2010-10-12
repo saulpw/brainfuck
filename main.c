bd93fb7ab71c6fa3838333ff62718926eaa7367d
saul <saul@ef3b9ed0-3d08-0410-9d73-8659cd293b64>
2010-10-12 06:00:52 +0000

    it works again
    git-svn-id: svn+ssh://pwanson.com/var/home/svn/sitelabor/projects/brainfuck@487 ef3b9ed0-3d08-0410-9d73-8659cd293b64

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 #include <getopt.h>
 #include <memory.h>
 
-char program[60000] = { 0 };
-char *data;
-char *ptr;
-char *ip;
-int ncode;
-int ndata;
+char code[30000] = { 0 };
+char data[30000] = { 0 };
+int dp = 0;
+int ip = 0;
+int ncode = 0;
 int debug = 0;
 int verbose = 0;
 
 void dump_state()
 {
     int i;
-    printf("IP=%ld/%d DP=%ld/%d  ", ip - program, ncode, ptr - data, ndata);
+    printf("IP=%d %c  DP=%d  ", ip, code[ip], dp);
     for (i=0; i < 10; i++)
         printf("% 3d ", data[i]);
 
@@ -24,9 +23,8 @@ void dump_state()
 
 void check_state()
 {
-    if (ip < program || ip > &program[ncode]
-     || ptr < data || ptr > &data[ndata]
-     || ndata >= sizeof(program) - ncode)
+    if (ip < 0 || ip > ncode
+     || dp < 0 || dp > sizeof(data))
     { 
         dump_state(); 
         assert(0); 
@@ -36,83 +34,86 @@ void check_state()
 
 void advance_data(int n)
 {
-    ptr += n;
-    if (ptr - data > ndata) {
-        ndata = ptr - data;
-        if (debug) { printf("[first use]"); dump_state(); }
-    }
+    dp += n;
 }
 
 void incr_data(int n)
 {
-    *ptr += n;
+    data[dp] += n;
 }
 
 void out_comment()
 {
     if (verbose)
-        fprintf(stderr, "%c", *ip);
+        fprintf(stderr, "%c", code[ip]);
 }
 
 void out_data()
 {
-    printf("%c", *ptr);
+    printf("%c", data[dp]);
 }
 
 void in_data()
 {
-    *ptr = getchar();
+    data[dp] = getchar();
 }
 
-void comment()
+#ifdef RECURSIVE_SCAN
+int scan(const char *p, int start, int dir, char match, char recurse)
 {
+    int idx = start;
     do {
-        ip++;
-        if (*ip == '(') 
-            comment();
-    } while (*ip != ')');
-    ip++;
-}
+        assert (idx >= 0);
+        idx += dir;
 
-// dir = 1 or -1
-int find(const char *p, int dir, char c, char recurse)
+        if (p[idx] == recurse)
+            idx = scan(p, idx, dir, match, recurse) + dir;
+    } while (p[idx] != match);
+
+    return idx;
+}
+#else
+int scan(const char *p, int start, int dir, char match, char recurse)
 {
-    int n = 0;
+    int idx = start;
+    int n = 1;
     do {
-        n += dir;
-        if (p[n] == recurse)
-            find(p + n, dir, c, recurse);
-    } while (p[n] != c);
+        assert (idx >= 0);
+        idx += dir;
 
-    return n * dir;
+        if (p[idx] == recurse)    ++n;
+        else if (p[idx] == match) --n;
+    } while (n != 0);
+
+    return idx;
 }
+#endif
 
-void scan_ip(int dirlevel, char recurse, char match)
+void skip_if_zero()
 {
-    int dir = dirlevel > 0 ? 1 : -1;
-    do {
-        ip += dir;
-        check_state();
-
-        if (*ip == recurse)
-            scan_ip(dirlevel + dir, recurse, match);
-    } while (*ip != match);
+    if (data[dp] == 0)
+        ip = scan(code, ip, 1, ']', '[');
 }
 
-#define EXTRA
+void loop_if_nonzero()
+{
+    if (data[dp] != 0) 
+        ip = scan(code, ip, -1, '[', ']');
+}
 
 void execute()
 {
-    data = &program[ncode];
+    bzero(data, sizeof(data));
 
-    ptr = data;
-    ip = program;
-    ndata = 0;
+    dp = 0;
+    ip = 0;
 
     do { 
-        if (debug && verbose) printf("%c", *ip);
+        check_state();
 
-        switch (*ip)
+        if (debug) dump_state();
+
+        switch (code[ip])
         {
         case '>': advance_data(1); break;
         case '<': advance_data(-1); break;
@@ -120,31 +121,18 @@ void execute()
         case '-': incr_data(-1); break;
         case '.': out_data(); break;
         case ',': in_data(); break;
-        case '[': if (*ptr == 0) { ip += find(ip, 1, ']', '['); } break;
-        case ']': if (*ptr != 0) { ip += find(ip, -1, '[', ']'); } break;
-#ifdef EXTRA
+        case '[': skip_if_zero(); break;
+        case ']': loop_if_nonzero(); break;
         case '#': dump_state(); break;
-        case '(': comment(); break;
-        case '1': 
-        case '2': 
-        case '3': 
-        case '4': 
-        case '5': 
-        case '6': 
-        case '7': 
-        case '8': 
-        case '9': *ptr += *ip - '0'; break;
-#endif
         default:  out_comment(); break;
         };
 
         ip++;
-        check_state();
-    } while (*ip != 0);
+
+    } while (code[ip] != 0);
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int opt;
     while ((opt = getopt(argc, argv, "dv")) != -1) 
@@ -166,12 +154,13 @@ main(int argc, char *argv[])
             continue;
         }
        
-        bzero(program, sizeof(program));
+        bzero(code, sizeof(code));
+
         ncode = 0;
 
         while (! feof(fp)) 
         {
-            program[ncode++] = fgetc(fp);
+            code[ncode++] = fgetc(fp);
         }
 
         fclose(fp);
